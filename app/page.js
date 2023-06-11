c2011570f6c828f5f24adfed131c83bf57e8823c
Evelyn2w <133771589+Evelyn2w@users.noreply.github.com>
2023-06-11 17:36:00 +0500

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,93 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+         <a
+            href="https://gosloto.co.za/"
+          >
+            <p>gosloto.co.za</p>
+          </a>
+        <div>
+          <a
+            href="https://lunchtimeprediction.com/"
+          >
+            <p>Lunchtime Prediction</p>
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="#"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Digital Marketing <span>-&gt;</span>
+          </h2>
+          <p>We are a team of 51 developers that do the test, develop and pulish many blog. We are 
+           highly experienced in the javascript and our create website are mentioned above.</p>
+        </a>
+
+        <a
+          href="#"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Backlink Information <span>-&gt;</span>
+          </h2>
+          <p>Most the search console owners demands to put their website link to the most reputable
+          blogs. So, we have linkback to many websites as well.!</p>
+        </a>
+
+        <a
+          href="#"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Local SEO <span>-&gt;</span>
+          </h2>
+          <p>As we a full digital marketing company so we do local seo projects as well. We have served 500+ clients
+          in the last 2 years. Google my business can help to get more traffic if you want work with local clients.</p>
+        </a>
+
+        <a
+          href="#"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Fiverr and Upwork <span>-&gt;</span>
+          </h2>
+          <p>
+            People can find us on the fiverr and upwork that are very reliable and reputed plateform to get any services as well.
+           Welcome to all customers that want to join us. Thank You.
+          </p>
+        </a>
+      </div>
+      <h2>
+           Learn How to Run Digital Marketing Company? <span>-&gt;</span>
+          </h2>
+          <p>Running a successful digital marketing company requires a deep understanding of the online landscape, strategic thinking, and the ability to adapt to ever-changing trends. In this article, we will explore the key aspects of running a digital marketing company and provide valuable insights to help you navigate this dynamic industry.</p>
+
+  <h2>1. Understanding Digital Marketing</h2>
+  <p>Before venturing into running a digital marketing company, it is crucial to have a strong grasp of the fundamentals. Digital marketing encompasses a wide range of strategies, including search engine optimization (SEO), pay-per-click (PPC) advertising, social media marketing, content marketing, email marketing, and more. Familiarize yourself with these concepts, as well as the tools and platforms commonly used in digital marketing campaigns.</p>
+
+  <h2>2. Define Your Niche</h2>
+  <p>Identifying a niche within the digital marketing industry can help you stand out from the competition. Consider specializing in a specific sector, such as e-commerce, healthcare, or real estate, or focus on a particular marketing strategy where you have expertise. By positioning yourself as an expert in a niche, you can attract clients who are looking for specialized services.</p>
+
+  <h2>3. Build a Strong Team</h2>
+  <p>Running a digital marketing company is not a one-person show. Assemble a team of skilled professionals who complement each other's strengths. Look for individuals with expertise in areas like SEO, content creation, social media management, graphic design, and analytics. Each team member should bring unique skills to the table, fostering a collaborative environment.</p>
+
+    </main>
+  )
+}
